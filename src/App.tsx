9ea42fb8cc5a46258eea9b1573ccb2bcfe84f9df
Emil Nilsson <emil.nilsson@protonmail.com>
2023-03-31 21:09:25 +0200

    Create `useToken` hook for token context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  createContext,
+  Dispatch,
+  FunctionComponent,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import LoginForm from "./loginform";
+
+export const TokenContext = createContext({
+  token: "",
+  setToken: (() => "") as Dispatch<SetStateAction<string>>,
+});
+
+export const TokenProvider: FunctionComponent<any> = ({ children }) => {
+  const [token, setToken] = useState<string>("");
+
+  return (
+    <TokenContext.Provider value={{ token, setToken }}>
+      {children}
+    </TokenContext.Provider>
+  );
+};
+
+export const useToken = () => useContext(TokenContext);
 
 function App() {
   return (
-    <div>
+    <TokenProvider>
       <LoginForm />
       <QuestionList />
       <QuestionForm />
-    </div>
+    </TokenProvider>
   );
 }
 
@@ -17,97 +43,18 @@ type Question = {
   questioner: string;
 };
 
-const LoginForm = () => {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [error, setError] = useState<Error | null>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [token, setToken] = useState<String>("");
-  const [hasSubmitted, setHasSubmitted] = useState(false);
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    authenticate();
-  };
-
-  const authenticate = async () => {
-    setIsLoaded(false);
-    setError(null);
-    setHasSubmitted(true);
-    const response = await fetch("http://localhost:8080/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Basic " + btoa(username + ":" + password),
-      },
-    });
-
-    if (response.status === 401) {
-      setError(Error("Not authenticated."));
-    } else if (response.status === 200) {
-      setToken((await response.json()).token);
-    }
-    setIsLoaded(true);
-  };
-
-  const isWaiting = hasSubmitted && !isLoaded;
-  const showError = hasSubmitted && isLoaded && error;
-
-  const form = (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Summary:
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <label>
-        Description:
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <input type="submit" value="Submit" disabled={isWaiting} />
-    </form>
-  );
-
-  const isAuthenticated = isLoaded && hasSubmitted && !error;
-
-  console.log(
-    "isAuthenticated",
-    isAuthenticated,
-    "isLoaded",
-    isLoaded,
-    "error",
-    error,
-    "hasSubmitted",
-    hasSubmitted
-  );
-  if (isAuthenticated) {
-    return <p>You have authenticated and received a token: {token}</p>;
-  }
-
-  return (
-    <div>
-      {form}
-      {showError && <div>{error.message}</div>}
-    </div>
-  );
-};
-
 function QuestionForm() {
+  const { token } = useToken();
   const [summary, setSummary] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [error, setError] = useState<Error | null>(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [question, setQuestion] = useState<Question | null>(null);
   const [hasSubmitted, setHasSubmitted] = useState(false);
+
+  if (token == "") {
+    return <p>QuestionFrom disabled. User is not authenticated.</p>;
+  }
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
---
+++ src/loginform.tsx
@@ -0,0 +1,88 @@
+import { useContext, useState } from "react";
+import { useToken } from "./App";
+
+const LoginForm = () => {
+  const { token, setToken } = useToken();
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    authenticate();
+  };
+
+  const authenticate = async () => {
+    setError(null);
+    setHasSubmitted(true);
+    setIsLoaded(false);
+    const response = await fetch("http://localhost:8080/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic " + window.btoa(username + ":" + password),
+      },
+    });
+
+    if (response.status === 401) {
+      setError(Error("Not authenticated."));
+    } else if (response.status === 200) {
+      setToken((await response.json()).token);
+    }
+    setIsLoaded(true);
+  };
+
+  const isWaiting = hasSubmitted && !isLoaded;
+  const showError = hasSubmitted && isLoaded && error;
+
+  const form = (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Summary:
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Description:
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" disabled={isWaiting} />
+    </form>
+  );
+
+  const isAuthenticated = isLoaded && hasSubmitted && !error;
+
+  console.log(
+    "isAuthenticated",
+    isAuthenticated,
+    "isLoaded",
+    isLoaded,
+    "error",
+    error,
+    "hasSubmitted",
+    hasSubmitted
+  );
+  if (isAuthenticated) {
+    return <p>You have authenticated and received a token: {token}</p>;
+  }
+
+  return (
+    <div>
+      {form}
+      {showError && <div>{error.message}</div>}
+    </div>
+  );
+};
+
+export default LoginForm;
